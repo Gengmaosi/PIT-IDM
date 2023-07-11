2d681b6d566b8971d3cbf13ca710cbf2954cb48b
Gengmaosi <55313040+Gengmaosi@users.noreply.github.com>
2023-07-11 15:58:56 +0800

    Update predict.py

--- PIT-IDM/predict.py
+++ PIT-IDM/predict.py
@@ -37,40 +37,13 @@ input_hist = input_hist/(max_num-min_num)
 input_x = torch.tensor(input_x).unsqueeze(0).to(torch.float32)
 input_hist = torch.tensor(input_hist).unsqueeze(0).to(torch.float32)
 
-ninput = 1
-ntoken = 1
-ninp = 14
-nhead = 2
-nhid = 28
-fusion_size = 3
-nlayers = 3
-dropout = 0.1
-output_length = 30
-s_0 = 1.667/(max_num-min_num)
-T = 0.504
-a = 0.430/(max_num-min_num)
-b = 3.216/(max_num-min_num)
-v_d = 16.775/(max_num-min_num)
-dt = 0.1
-lr_PUNN = 0.0005
-lr_PINN = 0.0000001
-epoch_num = 10
-alpha = 0.7
-model_1 = PIT_IDM_1(ninput, ntoken, ninp, nhead, nhid, fusion_size, nlayers, dropout, output_length, s_0, T, a, b, v_d, dt, lr_PUNN, lr_PINN, epoch_num, alpha)
-模型预测过程
-model_location = r'\PIT-IDM_1_UTE.tar'
-outputs = model_1.predict(input_x, input_hist, model_location)
-output = outputs.detach().numpy()
-output = output*(max_num-min_num)+min_num
-print(output, output.shape)
-
 # ninput = 1
 # ntoken = 1
-# ninp = 50
-# nhead = 10
-# nhid = 50
+# ninp = 14
+# nhead = 2
+# nhid = 28
 # fusion_size = 3
-# nlayers = 2
+# nlayers = 3
 # dropout = 0.1
 # output_length = 30
 # s_0 = 1.667/(max_num-min_num)
@@ -81,17 +54,44 @@ print(output, output.shape)
 # dt = 0.1
 # lr_PUNN = 0.0005
 # lr_PINN = 0.0000001
-# epoch_num = 500
+# epoch_num = 10
 # alpha = 0.7
-# model_2 = PIT_IDM_2(ninput, ntoken, ninp, nhead, nhid, fusion_size, nlayers, dropout, output_length, s_0, T, a, b, v_d, dt, lr_PUNN, lr_PINN, epoch_num, alpha)
-# model_location = r'\PIT-IDM(2)_CKQ4_0917.tar'
-# start_of_seq = torch.Tensor([0]).unsqueeze(0).unsqueeze(1).repeat(input_x.shape[0], 1, 1)
-# dec_input = start_of_seq
-# for i in range(output_length):
-#     target_mask = _generate_square_subsequent_mask(dec_input.shape[1])
-#     outputs = model_2.predict(input_x, dec_input, target_mask, input_hist, model_location)
-#     dec_input = torch.cat((dec_input, outputs[:, -1:, :]), 1)
+# model_1 = PIT_IDM_1(ninput, ntoken, ninp, nhead, nhid, fusion_size, nlayers, dropout, output_length, s_0, T, a, b, v_d, dt, lr_PUNN, lr_PINN, epoch_num, alpha)
+# 模型预测过程
+# model_location = r'\PIT-IDM_1_UTE.tar'
+# outputs = model_1.predict(input_x, input_hist, model_location)
 # output = outputs.detach().numpy()
 # output = output*(max_num-min_num)+min_num
 # print(output, output.shape)
+
+ninput = 1
+ntoken = 1
+ninp = 50
+nhead = 10
+nhid = 50
+fusion_size = 3
+nlayers = 2
+dropout = 0.1
+output_length = 30
+s_0 = 1.667/(max_num-min_num)
+T = 0.504
+a = 0.430/(max_num-min_num)
+b = 3.216/(max_num-min_num)
+v_d = 16.775/(max_num-min_num)
+dt = 0.1
+lr_PUNN = 0.0005
+lr_PINN = 0.0000001
+epoch_num = 500
+alpha = 0.7
+model_2 = PIT_IDM_2(ninput, ntoken, ninp, nhead, nhid, fusion_size, nlayers, dropout, output_length, s_0, T, a, b, v_d, dt, lr_PUNN, lr_PINN, epoch_num, alpha)
+model_location = r'\PIT-IDM(2)_CKQ4_0917.tar'
+start_of_seq = torch.Tensor([0]).unsqueeze(0).unsqueeze(1).repeat(input_x.shape[0], 1, 1)
+dec_input = start_of_seq
+for i in range(output_length):
+    target_mask = _generate_square_subsequent_mask(dec_input.shape[1])
+    outputs = model_2.predict(input_x, dec_input, target_mask, input_hist, model_location)
+    dec_input = torch.cat((dec_input, outputs[:, -1:, :]), 1)
+output = outputs.detach().numpy()
+output = output*(max_num-min_num)+min_num
+print(output, output.shape)
 # print(max_num,min_num)
